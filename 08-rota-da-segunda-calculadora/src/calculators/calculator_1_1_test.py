d260e81381abd8913b33b4cc6a2bc7f861a61870
taiane94sb <taiane19sb94@gmail.com>
2024-06-25 20:12:47 -0300

    Rota da Segunda Calculadora

---
+++ 08-rota-da-segunda-calculadora/src/calculators/calculator_1_1_test.py
@@ -0,0 +1,10 @@
+def test_something1():
+    print()
+    print('Teste 01')
+    pass
+
+
+def test_something2():
+    print()
+    print('Teste 02')
+    pass
---
+++ 08-rota-da-segunda-calculadora/src/main/routes/calculators.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, jsonify, request
+from src.calculators.calculator_1 import Calculator1
+from src.calculators.calculator_2 import Calculator2
+from src.drivers.numpy_handler import NumpyHandler
+
+
+calc_route_bp = Blueprint("calc_routes", __name__)
+
+
+@calc_route_bp.route("/calculator/1", methods=["POST"])
+def calculator_1():
+    calc = Calculator1()
+    response = calc.calculate(request)
+
+    return jsonify(response), 200
+
+
+@calc_route_bp.route("/calculator/2", methods=["POST"])
+def calculator_2():
+    numpyHandler = NumpyHandler()
+    calc = Calculator2(numpyHandler)
+    response = calc.calculate(request)
+
+    return jsonify(response), 200
