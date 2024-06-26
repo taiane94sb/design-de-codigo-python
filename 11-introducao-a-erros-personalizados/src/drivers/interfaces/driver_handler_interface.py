5f7f1d9a7bb670880da58217a13dcecf547f3ccd
taiane94sb <taiane19sb94@gmail.com>
2024-06-26 20:00:24 -0300

    Tratamento de Erros

---
+++ 11-introducao-a-erros-personalizados/src/drivers/interfaces/driver_handler_interface.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractclassmethod
+from typing import List
+
+
+class DriverHandlerInterface(ABC):
+
+    @abstractclassmethod
+    def standard_derivation(self, number: List[float]) -> float:
+        pass
+
+    @abstractclassmethod
+    def variance(self, number: List[float]) -> float:
+        pass
